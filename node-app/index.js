8d5f2a9931e95ecd669da6e430af19e3626e25be
SHUBHAM SHARMA <54950485+ShubhamSharma1560@users.noreply.github.com>
2021-12-26 18:23:19 +0530

    Add files via upload

--- node-app/index.js
+++ node-app/index.js
@@ -1,16 +1,24 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-const db = require('./models/init')
-const router = require('./routes');
-var cors = require('cors')
+var chat = require("./chat");
+var socketio = require("socket.io");
+var cors = require("cors");
+const app = require("express")();
+const http = require("http");
+const PORT = 8080;
 
 app.use(cors());
+/**
+ * Create HTTP server.
+ */
 
-app.use(router);
+var server = http.createServer(app);
+var io = socketio(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+server.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
+});
 
-db(function(err){
-    if(!err){
-        app.listen(port, () => console.log(`listening on port ${port}!`));
-    }
-})
+chat(io);
